0748bd80be53b8f94e494515b8fdc271fd31b165
marvingitau <marvingitau96@gmail.com>
2022-05-22 14:33:37 +0300

    remove cart stage to order u go direct to payment

--- app/Http/Controllers/Employer/CartController.php
+++ app/Http/Controllers/Employer/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Employer;
 
+use Carbon\Carbon;
 use Darryldecode\Cart\Cart;
 use Illuminate\Http\Request;
 use App\Models\Employer\Order;
@@ -103,10 +104,90 @@ class CartController extends Controller
                     )
                 ),
             ));
-            return back()->with('message','Added To Cart ');
+
+            /**
+             *
+             * ************************** ORDERCONTROLLER CREATE FUNCTION FOR THE AVAIL OF PAYMENT **************************************
+             *
+             */
+
+            //  Binds the cart to a specific user.
+            $userId = auth()->user()->id;
+            \Cart::session($userId);
+
+            // check whether you have tokens if not get redirected to buy them elsif purchase the product consuming the token
+            //*  when you buy a token it has expiry date and the amount
+            //* I must check for both
+            //*
+
+            $user_data = auth()->user();
+
+            $orderItems= ["data"=>$request->all()];
+            // $data= Cart::where('session_id',Session::get('session_id'))->get();
+
+            // Getting cart's contents for a specific user
+            $userId = auth()->user()->id; // or any string represents user identifier
+            $data=\Cart::getContent()->toArray();
+
+            $now = Carbon::now();
+            $trackingid =date('YmdHis');
+
+            foreach ($data as $key => $value) {
+                //What is the use of checking for duplication?
+                $order = Order::where('package', $value['attributes']['package'])->where('session_id',Session::get('emp_session_id'))->where('user_id',$userId)->where('quantity',$value['quantity'])->where('status',0)->where('order_verify',0)->first();
+
+                if ($order != null) {
+
+                    $order->update(
+                        [
+                            'user_id'=>$user_data->id,
+                            'users_email'=>$user_data->email,
+                            'payment_method'=>"NA",
+                            'quantity'=>$value['quantity'],
+                            'prodid'=>$value['attributes']['prodid'],
+                            'grand_total'=>(($value['price'])*(env('VAT')/100)+($value['price']) ),
+                            'order_verify'=>0,
+                            'trackingid'=>$trackingid,
+                            'expiry_date'=>Carbon::now()->addDays($value['attributes']['duration'])->format('Y-m-d H:i:s'),
+                            'session_id'=>Session::get('emp_session_id'),
+                        ]
+                    );
+
+                } else {
+
+                    $createdOrd=Order::create([
+
+                        'package'=>$value['attributes']['package'],
+                        'user_id'=>$user_data->id,
+                        'users_email'=>$user_data->email,
+                        'payment_method'=>"NA",
+                        'quantity'=>$value['quantity'],
+                        'prodid'=>$value['attributes']['prodid'],
+                        'grand_total'=>(($value['price'])*(env('VAT')/100)+($value['price']) ),
+                        'order_verify'=>0,
+                        'trackingid'=>$trackingid,
+                        'expiry_date'=>Carbon::now()->addDays($value['attributes']['duration'])->format('Y-m-d H:i:s'),
+                        'session_id'=>Session::get('emp_session_id'),
+
+                    ]);
+
+                }
+                // Order::updateOrCreate
+
+
+            }
+            \Cart::session($userId)->clear();
+            if(auth()->user()->role == 'employer'){
+                return redirect()->to('employer/payment');
+            }else{
+                return redirect()->to('candidate/payment');
+            }
+
+
+            // return back()->with('message','Added To Cart ');
 
         } catch (\Throwable $th) {
-            return back()->with('message','End Error Occurred'.$th);
+            return back()->with('message',$th->getMessage());
         }
 
 
